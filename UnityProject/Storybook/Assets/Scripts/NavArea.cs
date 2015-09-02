920c5b370929df75788365c6312517eae4579b17
nmbryant <nmbryant@wpi.edu>
2015-09-02 16:54:02 -0400

    Task #4: Comments the code for NavNode and NavArea, remove unneccessary lines. Adds another test for the nav system, but currently does not work due to problems with nodes detecting neighbors.
    Tested by: Running the test scenes

--- UnityProject/Storybook/Assets/Scripts/NavArea.cs
+++ UnityProject/Storybook/Assets/Scripts/NavArea.cs
@@ -5,14 +5,18 @@ public class NavArea : MonoBehaviour {
 
 	public NavNode start;
 	public NavNode dest;
+	public bool isTest;
 
 	List<NavNode> areaNodes = new List<NavNode>();
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log("NavArea Starting up");
-		bool areAllNodesStarted = false;
 		NavNode[] allNodes = FindObjectsOfType(typeof(NavNode)) as NavNode[];
+		// Initialize all of the nodes
+		for (int i = 0; i < allNodes.Length; i++){
+			allNodes[i].initializeNode();
+		}
 		Debug.Log("Area nodes length = " + areaNodes.Count);
 		List<NavNode> path = aStarSearch(start, dest);
 		for (int i = 0; i < path.Count; i++){
@@ -26,7 +30,9 @@ public class NavArea : MonoBehaviour {
 	
 	}
 
+	// Finds a path between the start node and the destination node using A* pathfinding algorithm
 	List<NavNode> aStarSearch(NavNode start, NavNode destination){
+		// Initialize the necessary lists and variables for A* pathfinding
 		List<NavNode> all = new List<NavNode>();
 		all.Add(start);
 		List<NavNode> closed = new List<NavNode>();
@@ -41,6 +47,7 @@ public class NavArea : MonoBehaviour {
 			Debug.Log ("open[0] ID = " + open[0].nodeId);
 			NavNode lowest = open[0];
 
+			// Find the node with the lowest value in open
 			for (int i = 0; i < open.Count; i++){
 				NavNode currentNode = open[i];
 				if (currentNode.getCostToHere() < lowest.getCostToHere()){
@@ -48,12 +55,15 @@ public class NavArea : MonoBehaviour {
 				}
 			}
 
+			// Remove the current lowest from open and add it to the closed since it is being visited
 			open.Remove (lowest);
 			closed.Add (lowest);
 
+			// Iterate through the neighbors of the selected node
 			for (int i = 0; i < lowest.getNeighborNodes().Count; i++){
 				Debug.Log ("Going through neighbors of node " + lowest.nodeId.ToString ());
 				NavNode currentNeighbor = lowest.getNeighborNodes()[i];
+				// Calculate the cost as the distance between the current node and the current neighbor
 				float cost = lowest.getCostToHere() + Vector3.Distance(lowest.transform.position, currentNeighbor.transform.position);
 				if (open.Contains(currentNeighbor) && cost < currentNeighbor.getCostToHere()) {
 					Debug.Log ("Removing neighbor from open");
@@ -66,10 +76,12 @@ public class NavArea : MonoBehaviour {
 				if (!open.Contains(currentNeighbor) && !closed.Contains(currentNeighbor)) {
 					currentBest = cost;
 					Debug.Log ("Open size = " + open.Count.ToString());
+					// If the open list is empty, add the current neighbor to the open list
 					if (open.Count == 0){
 						Debug.Log("Adding neighbor to open");
 						open.Add(currentNeighbor);
 					}
+					// Otherwise, place the node in the open list at the appropriate position according to its cost
 					else {
 						for (int j = 0; j < open.Count; j++){
 							if (cost < open[j].getCostToHere()){
@@ -78,12 +90,15 @@ public class NavArea : MonoBehaviour {
 							}
 						}
 					}
+
+					// Set the cost and parent of the current neighbor
 					currentNeighbor.setCostToHere(cost);
 					currentNeighbor.setParentNode(lowest);
 				}
 			}
 		}
 
+		// Once finished, determine what the path is by looking at the parent nodes and insert them into the path in the proper order
 		NavNode node = destination;
 		while (node.getParentNode() != start){
 			path.Insert(0, node);
@@ -93,6 +108,7 @@ public class NavArea : MonoBehaviour {
 		return path;
 	}
 
+	// Adds a NavNode the NavArea nodes list
 	public void addNode(NavNode nodeToAdd){
 		areaNodes.Add(nodeToAdd);
 	}
--- UnityProject/Storybook/Assets/Scripts/NavNode.cs
+++ UnityProject/Storybook/Assets/Scripts/NavNode.cs
@@ -2,43 +2,54 @@
 using System.Collections.Generic;
 
 public class NavNode : MonoBehaviour {
-
-	public bool isStarted = false;
+	
 	public int nodeId;
 	List<NavNode> neighborNodes = new List<NavNode>();
 	float costToHere = 0;
 	NavNode parentNode;
+	bool isStart = true;
 
 	// Use this for initialization
 	void Start () {
-		findNeighbors();
-		Debug.Log ("Node " + nodeId.ToString () + " has " + neighborNodes.Count.ToString () + " neighbors");
-		isStarted = true;
-		NavArea navArea = FindObjectOfType(typeof(NavArea)) as NavArea;
-		navArea.addNode(this);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
 
+	}
+	
+	// Uses raycasting to find all of the neigboring nodes and adds them to its neighbor list
 	void findNeighbors(){
 		NavNode[] allNodes = FindObjectsOfType(typeof(NavNode)) as NavNode[];
+		// Iterate through all of the NavNodes
 		for(int i = 0; i < allNodes.Length; i++){
 			Vector3 currentPosition = transform.position;
 			NavNode otherNode = allNodes[i];
+			// If the other node isn't this node, check to see if there is a clear path to it
 			if (otherNode != this){
-				RaycastHit hit;
 				Ray toOtherNodeRay = new Ray(transform.position, otherNode.transform.position);
-				Vector3	otherNodePosition = otherNode.transform.position;
-				if (!Physics.Raycast(toOtherNodeRay, out hit)){
+				// If nothing is hit by the raycast, add the other node to the neighbor list
+				if (!Physics.Raycast(toOtherNodeRay)){
 					neighborNodes.Add(otherNode);
 				}
 			}
 		}
 	}
 
+	// Initialize the node by finding its neighbors and executing other start up code
+	public void initializeNode(){
+		// At startup, find the neighbors of the node
+		findNeighbors();
+		Debug.Log ("Node " + nodeId.ToString () + " has " + neighborNodes.Count.ToString () + " neighbors");
+		for (int i = 0; i < neighborNodes.Count; i++){
+			Debug.Log ("Node " + nodeId.ToString() + " neighbor = " + neighborNodes[i].nodeId);
+		}
+		NavArea navArea = FindObjectOfType(typeof(NavArea)) as NavArea;
+		navArea.addNode(this);
+	}
+
+	// Getter and setter for costToHere
 	public float getCostToHere(){
 		return costToHere;
 	}
@@ -47,6 +58,7 @@ public class NavNode : MonoBehaviour {
 		costToHere = newCost;
 	}
 
+	// Getter and setter for parentNode
 	public NavNode getParentNode(){
 		return parentNode;
 	}
@@ -55,6 +67,7 @@ public class NavNode : MonoBehaviour {
 		parentNode = newParent;
 	}
 
+	// Getter for neighborNodes list
 	public List<NavNode> getNeighborNodes(){
 		return neighborNodes;
 	}

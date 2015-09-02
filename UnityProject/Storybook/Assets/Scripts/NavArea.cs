5d2ce09a80659a720942eea0dbe6ef14acefee11
nmbryant <nmbryant@wpi.edu>
2015-09-02 10:32:25 -0400

    Task #4: Creates the basis for the navigation system. Nodes use raycasting to find their neighbors. NavArea contains a list of nodes and uses A* pathfinding to find a path from two given points.
    Tested by: Running the project with the current debugs shows the path that was generated (2->3->4) which is what A* should produce

---
+++ UnityProject/Storybook/Assets/Scripts/NavArea.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class NavArea : MonoBehaviour {
+
+	public NavNode start;
+	public NavNode dest;
+
+	List<NavNode> areaNodes = new List<NavNode>();
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("NavArea Starting up");
+		bool areAllNodesStarted = false;
+		NavNode[] allNodes = FindObjectsOfType(typeof(NavNode)) as NavNode[];
+		Debug.Log("Area nodes length = " + areaNodes.Count);
+		List<NavNode> path = aStarSearch(start, dest);
+		for (int i = 0; i < path.Count; i++){
+			Debug.Log ("Node ID = " + path[i].nodeId);
+		}
+		Debug.Log("Path length = " + path.Count.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	List<NavNode> aStarSearch(NavNode start, NavNode destination){
+		List<NavNode> all = new List<NavNode>();
+		all.Add(start);
+		List<NavNode> closed = new List<NavNode>();
+		List<NavNode> open = new List<NavNode>(all);
+		List<NavNode> path = new List<NavNode>();
+		float totalCost = 0;
+		float currentBest = 0;
+
+		Debug.Log("Starting AStar");
+
+		while (open[0] != destination){
+			Debug.Log ("open[0] ID = " + open[0].nodeId);
+			NavNode lowest = open[0];
+
+			for (int i = 0; i < open.Count; i++){
+				NavNode currentNode = open[i];
+				if (currentNode.getCostToHere() < lowest.getCostToHere()){
+					lowest = currentNode;
+				}
+			}
+
+			open.Remove (lowest);
+			closed.Add (lowest);
+
+			for (int i = 0; i < lowest.getNeighborNodes().Count; i++){
+				Debug.Log ("Going through neighbors of node " + lowest.nodeId.ToString ());
+				NavNode currentNeighbor = lowest.getNeighborNodes()[i];
+				float cost = lowest.getCostToHere() + Vector3.Distance(lowest.transform.position, currentNeighbor.transform.position);
+				if (open.Contains(currentNeighbor) && cost < currentNeighbor.getCostToHere()) {
+					Debug.Log ("Removing neighbor from open");
+					open.Remove(currentNeighbor);
+				}
+				if (closed.Contains(currentNeighbor) && cost < currentNeighbor.getCostToHere()) {
+					Debug.Log ("Removing neighbor from closed");
+					closed.Remove(currentNeighbor);
+				}
+				if (!open.Contains(currentNeighbor) && !closed.Contains(currentNeighbor)) {
+					currentBest = cost;
+					Debug.Log ("Open size = " + open.Count.ToString());
+					if (open.Count == 0){
+						Debug.Log("Adding neighbor to open");
+						open.Add(currentNeighbor);
+					}
+					else {
+						for (int j = 0; j < open.Count; j++){
+							if (cost < open[j].getCostToHere()){
+								open.Insert(j, currentNeighbor);
+								break;
+							}
+						}
+					}
+					currentNeighbor.setCostToHere(cost);
+					currentNeighbor.setParentNode(lowest);
+				}
+			}
+		}
+
+		NavNode node = destination;
+		while (node.getParentNode() != start){
+			path.Insert(0, node);
+			node = node.getParentNode();
+		}
+		path.Insert(0, node);
+		return path;
+	}
+
+	public void addNode(NavNode nodeToAdd){
+		areaNodes.Add(nodeToAdd);
+	}
+}
---
+++ UnityProject/Storybook/Assets/Scripts/NavNode.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class NavNode : MonoBehaviour {
+
+	public bool isStarted = false;
+	public int nodeId;
+	List<NavNode> neighborNodes = new List<NavNode>();
+	float costToHere = 0;
+	NavNode parentNode;
+
+	// Use this for initialization
+	void Start () {
+		findNeighbors();
+		Debug.Log ("Node " + nodeId.ToString () + " has " + neighborNodes.Count.ToString () + " neighbors");
+		isStarted = true;
+		NavArea navArea = FindObjectOfType(typeof(NavArea)) as NavArea;
+		navArea.addNode(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void findNeighbors(){
+		NavNode[] allNodes = FindObjectsOfType(typeof(NavNode)) as NavNode[];
+		for(int i = 0; i < allNodes.Length; i++){
+			Vector3 currentPosition = transform.position;
+			NavNode otherNode = allNodes[i];
+			if (otherNode != this){
+				RaycastHit hit;
+				Ray toOtherNodeRay = new Ray(transform.position, otherNode.transform.position);
+				Vector3	otherNodePosition = otherNode.transform.position;
+				if (!Physics.Raycast(toOtherNodeRay, out hit)){
+					neighborNodes.Add(otherNode);
+				}
+			}
+		}
+	}
+
+	public float getCostToHere(){
+		return costToHere;
+	}
+
+	public void setCostToHere(float newCost){
+		costToHere = newCost;
+	}
+
+	public NavNode getParentNode(){
+		return parentNode;
+	}
+
+	public void setParentNode(NavNode newParent){
+		parentNode = newParent;
+	}
+
+	public List<NavNode> getNeighborNodes(){
+		return neighborNodes;
+	}
+}

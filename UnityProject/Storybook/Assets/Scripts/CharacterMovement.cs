6828871e37af7bdbd497e2692f1f72b9d30619b1
nmbryant <nmbryant@wpi.edu>
2015-09-05 15:23:26 -0400

    Task #2: Creates a character that can be moved with either the arrow keys or WASD.
    Tested by: Running the character movement test scene and moving the character with both WASD and the arrow keys.

---
+++ UnityProject/Storybook/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+	
+	public float speed = 8.0F;
+	public float gravity = 30.0F;
+
+	private Vector3 m_moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+		m_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		m_moveDirection = transform.TransformDirection(m_moveDirection);
+		m_moveDirection *= speed;
+		m_moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(m_moveDirection * Time.deltaTime);
+	}
+}

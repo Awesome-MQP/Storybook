ccf5aaa28bad793eb747e85ff400c99b9d1e147f
nmbryant <nmbryant@wpi.edu>
2015-09-06 12:37:22 -0400

    Task #1: Fixes bug where character gets stuck if destination is too close to a wall. Fixed by checking the previous locations of the character and seeing if it has stopped moving.
    Tested by: Clicking a point close to the wall in the test character movement scene.

--- UnityProject/Storybook/Assets/Scripts/CharacterMovement.cs
+++ UnityProject/Storybook/Assets/Scripts/CharacterMovement.cs
@@ -13,10 +13,14 @@ public class CharacterMovement : MonoBehaviour {
 	private List<Vector3> m_characterPath = new List<Vector3>();
 	private int m_currentPathIndex;
 	private Vector3 m_targetPosition;
-	
+	private Vector3[] m_previousPositions = new Vector3[10];
+	private int m_prevPositionIndex = 0;
+
 	// Use this for initialization
 	void Start () {
-
+		for (int i = 0; i < 10; i++){
+			m_previousPositions[i] = Vector3.zero;
+		}
 	}
 	
 	// Update is called once per frame
@@ -25,36 +29,64 @@ public class CharacterMovement : MonoBehaviour {
 		if (!isTouchControls){
 			m_moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		}
+		// If it is touch controls and the mouse has been clicked, calculate the path for the character
 		if (isTouchControls && Input.GetMouseButtonDown(0)){
 			Debug.Log ("Calculating Path");
-			calculateCharacterPath();
-			m_isMoving = true;
+			_calculateCharacterPath();
+			if (m_characterPath.Count > 0){
+				m_isMoving = true;
+			}
 		}
+		// If the character path is not empty, move to the current destination
 		if (m_characterPath.Count > 0){
+			m_previousPositions[m_prevPositionIndex] = transform.position;
+			m_prevPositionIndex++;
+			bool hasStopped = true;
+			if (m_prevPositionIndex == m_previousPositions.Length){
+				m_prevPositionIndex = 0;
+				Vector3 previous;
+				previous = m_previousPositions[0];
+				for (int i = 1; i < m_previousPositions.Length; i++){
+					if (previous != m_previousPositions[i]){
+						hasStopped = false;
+					}
+					previous = m_previousPositions[i];
+				}
+			}
+			else {
+				hasStopped = false;
+			}
 			Vector3 currentDest = m_characterPath[m_currentPathIndex];
 			currentDest.y = transform.position.y;
 			m_moveDirection = currentDest - transform.position;
 			Debug.Log ("Distance = " + Vector3.Distance(currentDest, transform.position).ToString ());
-			if (Vector3.Distance(currentDest, transform.position) < 0.1){
+			// If the character is within range of the destination, increment te path index
+			if (Vector3.Distance(currentDest, transform.position) < 0.1 || hasStopped){
 				m_currentPathIndex += 1;
+				// If the character has reached the end of the path, reset the path and set isMoving to false
+				// since the character will be stopped
 				if (m_currentPathIndex == m_characterPath.Count){
 					m_characterPath = new List<Vector3>();
+					m_previousPositions = new Vector3[10];
 					m_isMoving = false;
 					Debug.Log ("Resetting List");
 				}
 			}
 		}
+		// Set the move direction based on speed, gravity and time if the character is moving or if it isn't touch controls
 		if (!isTouchControls || m_isMoving){
-			//m_moveDirection = transform.TransformDirection(m_moveDirection);
+			m_moveDirection = transform.TransformDirection(m_moveDirection);
 			m_moveDirection = m_moveDirection.normalized * speed;
 			m_moveDirection.y -= gravity * Time.deltaTime;
 			controller.Move(m_moveDirection * Time.deltaTime);
 		}
 	}
-	
-	private void calculateCharacterPath(){
+
+	// Used with touch controls, calculates a path the clicked location
+	private void _calculateCharacterPath(){
 		Camera mainCamera = FindCamera(); 
 
+		// Get the position of the click in the game world
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		Debug.DrawRay(ray.origin, ray.direction, Color.black, 10000);
 		Plane xy = new Plane(Vector3.up, new Vector3(0, 0, 0));
@@ -69,16 +101,34 @@ public class CharacterMovement : MonoBehaviour {
 		float distance = Vector3.Distance(transform.position, m_targetPosition);
 		Ray charToDestRay = new Ray(transform.position, direction);
 		Debug.DrawRay(transform.position, direction, Color.cyan, 10000);
+
+		GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall");
+		// Check to make sure that the point is not located in a wall
+		for (int i = 0; i < walls.Length; i++){
+			GameObject currentWall = walls[i];
+			BoxCollider wallCollider = currentWall.GetComponent(typeof(BoxCollider)) as BoxCollider;
+			if (wallCollider != null){
+				// If the clicked point is in a wall, the character can not move to here, so return
+				if (wallCollider.bounds.Contains(m_targetPosition)){
+					Debug.Log ("Can't move into wall");
+					return;
+				}
+			}
+		}
+		// If the character can move the clicked point without hitting anything, the path will just contain the destination
 		if (!Physics.Raycast(charToDestRay, distance)){
 			m_characterPath.Add(m_targetPosition);
 			Debug.Log ("Adding target position");
 		}
+		// Otherwise, need to use A* to determine a path
 		else {
 			NavNode closestDestNode = null;
 			float smallestDestDistance = -1;
 			NavNode closestCharacterNode = null;
 			float smallestCharacterDistance = -1;
 			List<NavNode> areaNodes = currentNavArea.getAreaNodes();
+			// Iterate through the area nodes to find the node closest to the character and the node
+			// closest to the destination
 			for (int i = 0; i < areaNodes.Count; i++){
 				NavNode currentNode = areaNodes[i];
 				float nodeToDest = Vector3.Distance(currentNode.transform.position, m_targetPosition);
@@ -94,10 +144,12 @@ public class CharacterMovement : MonoBehaviour {
 			}
 			Debug.Log ("Closest Node to character = " + closestCharacterNode.nodeId.ToString ());
 			Debug.Log ("Closest node to destination = " + closestDestNode.nodeId.ToString());
+			// Use A* to calculate a path
 			List<NavNode> characterNodePath = currentNavArea.AStarSearch(closestCharacterNode, closestDestNode);
 			if (characterNodePath.Contains(closestCharacterNode)){
 				characterNodePath.Insert (0, closestCharacterNode);
 			}
+			// Iterate through the path and add the positions of all the nodes to the character path
 			for (int i = 0; i < characterNodePath.Count; i++){
 				m_characterPath.Add(characterNodePath[i].transform.position);
 			}
@@ -105,7 +157,8 @@ public class CharacterMovement : MonoBehaviour {
 		}
 		m_currentPathIndex = 0;
 	}
-	
+
+	// Returns the camera
 	Camera FindCamera (){
 		if (GetComponent<Camera>())
 			return GetComponent<Camera>();

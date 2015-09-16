5a07841f7241f02514f0889d81bcb127f125e168
nmbryant <nmbryant@wpi.edu>
2015-09-15 20:20:22 -0400

    Task #21: Changes code to address critiques.
    Tested by: Running the scene and making sure that it still works.

--- UnityProject/Storybook/Assets/Scripts/CombatManager/CombatManager.cs
+++ UnityProject/Storybook/Assets/Scripts/CombatManager/CombatManager.cs
@@ -5,40 +5,29 @@ using System.Collections.Generic;
 public class CombatManager : MonoBehaviour {
 
     [SerializeField]
-    private GameObject m_combatPawnPrefab;
+    private TestCombatPawn m_combatPawnPrefab;
 
-    private Animator m_combatStateMachine;
     private ThinkState m_thinkState;
     private ExecuteState m_executeState;
     private WinState m_winState;
     private LoseState m_loseState;
 
-    private GameObject m_combatPawn;
+    private TestCombatPawn m_combatPawn;
     private int m_submittedMoves = 0;
     private List<CombatPawn> m_playerList = new List<CombatPawn>();
 
 	// Use this for initialization
 	void Start () {
-        m_combatStateMachine = GetComponent<Animator>() as Animator;
+        Animator m_combatStateMachine = GetComponent<Animator>() as Animator;
         m_combatStateMachine.SetTrigger("StartCombat");
 
-        // Get the ThinkState and set the CombatManager
-        m_thinkState = m_combatStateMachine.GetBehaviour<ThinkState>() as ThinkState;
-        m_thinkState.CManager = this;
+        CombatState[] allCombatStates = m_combatStateMachine.GetBehaviours<CombatState>() as CombatState[];
+        foreach (CombatState cm in allCombatStates)
+        {
+            cm.CManager = this;
+        }
 
-        // Get the ExecuteState and set the CombatManager
-        m_executeState = m_combatStateMachine.GetBehaviour<ExecuteState>() as ExecuteState;
-        m_executeState.CManager = this;
-
-        // Get the WinState and set the CombatManager
-        m_winState = m_combatStateMachine.GetBehaviour<WinState>() as WinState;
-        m_winState.CManager = this;
-
-        // Get the LoseState and set the Combat Manager
-        m_loseState = m_combatStateMachine.GetBehaviour<LoseState>() as LoseState;
-        m_loseState.CManager = this;
-
-        m_combatPawn = (GameObject) Instantiate(m_combatPawnPrefab);
+        m_combatPawn = Instantiate(m_combatPawnPrefab);
         CombatPawn combatPawnScript = m_combatPawn.GetComponent<CombatPawn>();
         combatPawnScript.SetCombatManager(this);
         m_playerList.Add(combatPawnScript);

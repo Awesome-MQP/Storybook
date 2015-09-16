a7937569bec7764f486c1f390e79c3e84c16341c
nmbryant <nmbryant@wpi.edu>
2015-09-15 21:12:06 -0400

    Task #17: Updates CombatUI prefab to include everything necessary for a combat. Creates a CombatUIHandler class to handle the button presses of the combat UI. Tested by: Running the scene and pressing the buttons.

---
+++ UnityProject/Storybook/Assets/Scripts/CombatManager/CombatUIHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatUIHandler : MonoBehaviour {
+
+    public void PageButtonPressed()
+    {
+        Debug.Log("Page button pressed");
+    }
+
+    public void ItemButtonPressed()
+    {
+        Debug.Log("Item button pressed");
+    }
+
+    public void InfoButtonPressed()
+    {
+        Debug.Log("Info button pressed");
+    }
+
+    public void RunButtonPressed()
+    {
+        Debug.Log("Run button pressed");
+    }
+}

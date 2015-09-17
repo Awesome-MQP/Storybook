0b87709213be8adcf088d286a958417882c60d8f
nmbryant <nmbryant@wpi.edu>
2015-09-17 18:21:35 -0400

    Task#17: Updates the prefabs for commit.
    Tested by: Running the test scene and verifying that it still works.

--- UnityProject/Storybook/Assets/Scripts/Game/GameManager/GameManager.cs
+++ UnityProject/Storybook/Assets/Scripts/Game/GameManager/GameManager.cs
@@ -8,7 +8,13 @@ public class GameManager : MonoBehaviour {
 
     private GameObject combatInstance;
     private int m_frameNumber = 1;
+    private Camera m_mainCamera;
 	
+    void Start()
+    {
+        m_mainCamera = Camera.main;
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (m_frameNumber == 100)
@@ -24,11 +30,19 @@ public class GameManager : MonoBehaviour {
 
     public void StartCombat()
     {
-        combatInstance = Instantiate(m_combatUIPrefab);
+        Vector3 currentCameraLocation = m_mainCamera.transform.position;
+        //Quaternion currentCameraRotation = m_mainCamera.transform.rotation;
+        //Vector3 newCameraPosition = new Vector3(currentCameraLocation.x - 100, currentCameraLocation.y, currentCameraLocation.z);
+        combatInstance = (GameObject) Instantiate(m_combatUIPrefab);
+        Camera combatCamera = combatInstance.GetComponentInChildren<Camera>();
+        combatCamera.enabled = true;
+        m_mainCamera.enabled = false;
     }
 
     public void EndCombat()
     {
+        m_mainCamera.enabled = true;
+        combatInstance.GetComponentInChildren<Camera>().enabled = false;
         Destroy(combatInstance);
     }
 }

9a307785c7d621781bee60686fc0a572d13a1546
nmbryant <nmbryant@wpi.edu>
2015-09-18 17:39:06 -0400

    Task #17: Changes the combat scene layout code and prefabs. Gets rid of the UI components, and makes a test that demonstrates multiple combat instances being spawned.
    Tested by: Running the test scene TestGameManager

--- UnityProject/Storybook/Assets/Scripts/Game/GameManager/GameManager.cs
+++ UnityProject/Storybook/Assets/Scripts/Game/GameManager/GameManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
     [SerializeField]
-    private GameObject m_combatUIPrefab;
+    private GameObject m_combatInstancePrefab;
 
-    private GameObject combatInstance;
+    [SerializeField]
+    private Vector3 m_defaultLocation;
+
     private int m_frameNumber = 1;
     private Camera m_mainCamera;
-	
+    private List<GameObject> m_combatInstances = new List<GameObject>();
+
     void Start()
     {
         m_mainCamera = Camera.main;
@@ -21,28 +24,37 @@ public class GameManager : MonoBehaviour {
         {
             StartCombat();
         }
+        if (m_frameNumber == 200)
+        {
+            StartCombat();
+        }
         else if (m_frameNumber == 300)
         {
-            EndCombat();
+            EndCombat(m_combatInstances[0].GetComponent<CombatManager>());
         }
         m_frameNumber++;
 	}
 
     public void StartCombat()
     {
-        Vector3 currentCameraLocation = m_mainCamera.transform.position;
-        //Quaternion currentCameraRotation = m_mainCamera.transform.rotation;
-        //Vector3 newCameraPosition = new Vector3(currentCameraLocation.x - 100, currentCameraLocation.y, currentCameraLocation.z);
-        combatInstance = (GameObject) Instantiate(m_combatUIPrefab);
+        Vector3 combatPosition = new Vector3(m_defaultLocation.x + 1000 * m_combatInstances.Count, m_defaultLocation.y + 1000 * m_combatInstances.Count,
+            m_defaultLocation.z + 1000 * m_combatInstances.Count);
+        GameObject combatInstance = (GameObject) Instantiate(m_combatInstancePrefab, combatPosition, Quaternion.identity);
+        m_combatInstances.Add(combatInstance);
         Camera combatCamera = combatInstance.GetComponentInChildren<Camera>();
-        combatCamera.enabled = true;
-        m_mainCamera.enabled = false;
     }
 
-    public void EndCombat()
+    public void EndCombat(CombatManager cm)
     {
-        m_mainCamera.enabled = true;
-        combatInstance.GetComponentInChildren<Camera>().enabled = false;
-        Destroy(combatInstance);
+        for (int i = 0; i < m_combatInstances.Count; i++)
+        {
+            GameObject currentCombatInstance = m_combatInstances[i];
+            CombatManager currentCombatManager = currentCombatInstance.GetComponent<CombatManager>();
+            if (currentCombatManager == cm)
+            {
+                Destroy(currentCombatInstance);
+                break;
+            }
+        }
     }
 }

b29f9a447b92266943fc0b03b503f04c2c382f95
cporell <cgporell@wpi.edu>
2015-10-08 20:49:02 -0400

    Task #45: Changed the damage formula to reflect the design changes from this week.

--- UnityProject/Storybook/Assets/Scripts/CombatManager/StatsManager.cs
+++ UnityProject/Storybook/Assets/Scripts/CombatManager/StatsManager.cs
@@ -3,53 +3,40 @@ using System.Collections;
 
 public class StatsManager : MonoBehaviour {
 
-    private float m_positiveWeaponBonus = 1.2f;
-    private float m_neutralWeaponBonus = 1f;
-    private float m_negativeWeaponBonus = 0.8f;
-    private float m_positiveSTABBonus = 1.6f;
-    private float m_neutralSTABBonus = 1f;
-    private float m_negativeSTABBonus = 0.6f;
-    private float m_positiveTypeMatchBonus = 1.5f;
-    private float m_neutralTypeMatchBonus = 1f;
-    private float m_negativeTypeMatchBonus = 0.5f;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private float m_positiveSTABBonus = 1f;
+    private float m_neutralSTABBonus = 0.5f;
+    private float m_negativeSTABBonus = 0.3f;
+    private float m_positiveTypeMatchBonus = 1f;
+    private float m_neutralTypeMatchBonus = 0.5f;
+    private float m_negativeTypeMatchBonus = 0.25f;
 
     /// <summary>
-    /// Does damage calculation based on the parameters sent in.
+    /// Does damage calculation. This version of the formula reflects the design change from late A-term.
     /// </summary>
-    /// <param name="attackerGenre">The Genre of the attacker; used to determine STAB.</param>
-    /// <param name="defenderGenre">The Genre of the defender; used to determine Type Advantage.</param>
-    /// <param name="attackerAttackGenre">The Genre of the attacker's attack move; used to determine STAB and Type Advantage.</param>
-    /// <param name="attackerWeaponGenre">The Genre of the attacker's weapon; used to determine Weapon Compatibility.</param>
-    /// <param name="attackerBaseStrength">The Str stat of the attacker.</param>
-    /// <param name="attackerWeaponStrength">The Str modifier of the attacker's weapon.</param>
-    /// <param name="defenderDefense">The Def stat of the target.</param>
+    /// <param name="attackerGenre">The Genre of the attacker.</param>
+    /// <param name="defenderGenre">The Genre of the defender.</param>
+    /// <param name="attackerMoveGenre">The Genre of the attacker's move.</param>
+    /// <param name="attackerStrBase">The base Str stat of the attacker.</param>
+    /// <param name="attackerStrBoost">The modifier to the attacker's Str based on boosts from combat.</param>
+    /// <param name="attackerStrMod">Modifier for the attacker's Str based on the pages in their hand.</param>
+    /// <param name="defenderDefBase">The base Def stat of the defender.</param>
+    /// <param name="defenderDefMod">The modifier to the defender's Def based on the pages in their hand.</param>
+    /// <param name="defenderDefBoost">Modifier for the defender's Def based on boosts from combat.</param>
     /// <returns></returns>
-    int DoDamage(Genre attackerGenre, Genre defenderGenre, Genre attackerAttackGenre, Genre attackerWeaponGenre, 
-                  float attackerBaseStrength, float attackerWeaponStrength, float defenderDefense)
+    int CalcDamage(Genre attackerGenre, Genre defenderGenre, Genre attackerMoveGenre, float attackerStrBase, float attackerStrBoost, float attackerStrMod,
+                   float defenderDefBase, float defenderDefMod, float defenderDefBoost)
     {
-        float sameTypeAttackBonus = GetSameTypeBonus(attackerGenre, attackerAttackGenre);
-        float weaponCompatibleBonus = GetWeaponCompatibilityBonus(attackerGenre, attackerWeaponGenre);
-        float typeAdvantageBonus = GetTypeAdvantageBonus(attackerAttackGenre, defenderGenre);
+        float totalAttack = attackerStrBase + attackerStrBoost + attackerStrMod;
+        float totalDef = defenderDefBase + defenderDefBoost + defenderDefMod;
+        float sameTypeMoveBonus = GetSameTypeBonus(attackerGenre, attackerMoveGenre);
+        float typeAdvantageBonus = GetTypeAdvantageBonus(attackerMoveGenre, defenderGenre);
 
-        float totalDmg = ((attackerWeaponStrength + attackerBaseStrength) * weaponCompatibleBonus * sameTypeAttackBonus * typeAdvantageBonus)
-            - (defenderDefense + 1);
+        float totalDmg = totalAttack * (sameTypeMoveBonus * typeAdvantageBonus) - totalDef;
 
-        if (totalDmg < 0)
+        if (totalDef < 0)
         { return 0; }
         else
-        {
-            return Mathf.FloorToInt(totalDmg);
-        }
+        { return Mathf.FloorToInt(totalDmg); }
     }
 
     /// <summary>
@@ -101,58 +88,6 @@ public class StatsManager : MonoBehaviour {
                 return 1f;
         }
     }
-
-    /// <summary>
-    /// Generates a bonus or penalty depending on the player's genre and that of their weapon.
-    /// This bonus is not as great as the STAB, but can stack with it.
-    /// </summary>
-    /// <param name="ofPlayer">Genre of the player.</param>
-    /// <param name="ofWeapon">Genre of the weapon.</param>
-    /// <returns>Bonus or penalty based on the two input genres.</returns>
-    float GetWeaponCompatibilityBonus(Genre ofPlayer, Genre ofWeapon)
-    {
-        switch (ofPlayer)
-        {
-            // Each case returns a Weapon Compatibility bonus based on how the weapon's type compares to the user's.
-            // Same type results in a 1.3x mult, the type that either counters or is countered by the player's type results
-            // in a 0.7x mult, and the last "neutral" type results in a 1x mult (no change).
-            case Genre.Fantasy:
-                if (ofWeapon == Genre.Fantasy)
-                { return m_positiveWeaponBonus; }
-                else if (ofWeapon == Genre.GraphicNovel)
-                { return m_neutralWeaponBonus; }
-                else
-                { return m_negativeWeaponBonus; }
-
-            case Genre.GraphicNovel:
-                if (ofWeapon == Genre.GraphicNovel)
-                { return m_positiveWeaponBonus; }
-                else if (ofWeapon == Genre.Fantasy)
-                { return m_neutralWeaponBonus; }
-                else
-                { return m_negativeWeaponBonus; }
-
-            case Genre.Horror:
-                if (ofWeapon == Genre.Horror)
-                { return m_positiveWeaponBonus; }
-                else if (ofWeapon == Genre.SciFi)
-                { return m_neutralWeaponBonus; }
-                else
-                { return m_negativeWeaponBonus; }
-
-            case Genre.SciFi:
-                if (ofWeapon == Genre.SciFi)
-                { return m_positiveWeaponBonus; }
-                else if (ofWeapon == Genre.Horror)
-                { return m_neutralWeaponBonus; }
-                else
-                { return m_negativeWeaponBonus; }
-
-            default:
-                return 1f;
-        }
-    }
-
 
     /// <summary>
     /// Given the player's weapon type and the type of the opponent, calculates the type advantage damage multiplier.

6fca6b5882e405ccb6a8a368aa650e28169c2c2d
bpeake13 <bennypeake@gmail.com>
2015-10-13 14:44:05 -0400

    Created Sync Test

---
+++ UnityProject/Storybook/Assets/Scripts/NetSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetSpawner : NetworkBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject newGameObject = Instantiate(m_prefab);
+            NetworkServerExtended.SpawnWithChildren(newGameObject);
+        }
+    }
+
+    [SerializeField]
+    private GameObject m_prefab;
+}

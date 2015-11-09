195c0da688d0d5d4fb00bcc92ad403547eac46d5
cporell <cgporell@wpi.edu>
2015-11-08 21:59:24 -0500

    Task-Network-Mgr: Made the Player Entity class Photon-compliable and also created a Network Manager that will spawn a player when they join a room.

--- UnityProject/Storybook/Assets/Scripts/Game/Player/PlayerEntity.cs
+++ UnityProject/Storybook/Assets/Scripts/Game/Player/PlayerEntity.cs
@@ -14,6 +14,30 @@ public class PlayerEntity : Photon.MonoBehaviour
         get { return m_name; }
     }
 
+    public Genre PlayerGenre
+    {
+        get { return m_playerGenre; }
+        set
+        {
+            if(PhotonNetwork.isMasterClient)
+            {
+                m_playerGenre = value;
+            }
+        }
+    }
+
+    public int HitPointsMod
+    {
+        get { return m_hitPointsMod; }
+        set
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                m_hitPointsMod = value;
+            }
+        }
+    }
+
     public int HitPoints
     {
         get { return m_hitPoints; }
@@ -33,6 +57,18 @@ public class PlayerEntity : Photon.MonoBehaviour
                 m_maxHitPoints = value;
         }
     }
+    
+    public int AttackMod
+    {
+        get { return m_attackMod; }
+        set
+        {
+            if(PhotonNetwork.isMasterClient)
+            {
+                m_attackMod = value;
+            }
+        }
+    }
 
     public int Attack
     {
@@ -44,6 +80,18 @@ public class PlayerEntity : Photon.MonoBehaviour
         }
     }
 
+    public int DefenseMod
+    {
+        get { return m_defenseMod; }
+        set
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                m_defenseMod = value;
+            }
+        }
+    }
+
     public int Defense
     {
         get { return m_defense; }
@@ -51,6 +99,18 @@ public class PlayerEntity : Photon.MonoBehaviour
         {
             if (PhotonNetwork.isMasterClient)
                 m_defense = value;
+        }
+    }
+
+    public int SpeedMod
+    {
+        get { return m_speedMod; }
+        set
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                m_speedMod = value;
+            }
         }
     }
 
@@ -80,33 +140,79 @@ public class PlayerEntity : Photon.MonoBehaviour
             CmdInternalRenameServer(newName);
     }
 
-    [Command]
+    [PunRPC]
     private void CmdInternalRenameServer(string newName)
     {
         m_name = newName;
     }
 
-    [SyncVar, SerializeField]
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.isWriting)
+        {
+            stream.SendNext(m_attack);
+            stream.SendNext(m_attackMod);
+            stream.SendNext(m_defense);
+            stream.SendNext(m_defenseMod);
+            stream.SendNext(m_hitPoints);
+            stream.SendNext(m_hitPointsMod);
+            stream.SendNext(m_maxHitPoints);
+            stream.SendNext(m_name);
+            stream.SendNext(m_playerGenre);
+            stream.SendNext(m_playerInventory);
+            stream.SendNext(m_speed);
+            stream.SendNext(m_speedMod);
+        }
+        else
+        {
+            this.m_attack = (int)stream.ReceiveNext();
+            this.m_attackMod = (int)stream.ReceiveNext();
+            this.m_defense = (int)stream.ReceiveNext();
+            this.m_defenseMod = (int)stream.ReceiveNext();
+            this.m_hitPoints = (int)stream.ReceiveNext();
+            this.m_hitPointsMod = (int)stream.ReceiveNext();
+            this.m_maxHitPoints = (int)stream.ReceiveNext();
+            this.m_name = (string)stream.ReceiveNext();
+            this.m_playerGenre = (Genre)stream.ReceiveNext();
+            this.m_playerInventory = (PlayerInventory)stream.ReceiveNext();
+            this.m_speed = (int)stream.ReceiveNext();
+            this.m_speedMod = (int)stream.ReceiveNext();
+        }
+    }
+
+    [SerializeField]
+    private Genre m_playerGenre = Genre.None;
+
+    [SerializeField]
     private string m_name = string.Empty;
 
-    [SyncVar, SerializeField]
+    [SerializeField]
     private int m_hitPoints = 10;
 
-    [SyncVar, SerializeField]
+    [SerializeField]
     private int m_maxHitPoints = 10;
 
-    [SyncVar, SerializeField]
+    [SerializeField]
+    private int m_hitPointsMod = 0;
+
+    [SerializeField]
+    private int m_attackMod = 0;
+
+    [SerializeField]
     private int m_attack = 1;
 
-    [SyncVar, SerializeField]
+    [SerializeField]
+    private int m_defenseMod = 0;
+
+    [SerializeField]
     private int m_defense = 1;
 
-    [SyncVar, SerializeField]
+    [SerializeField]
+    private int m_speedMod = 0;
+
+    [SerializeField]
     private int m_speed = 1;
 
-    [SyncVar, SerializeField]
-    private int m_luck = 1;
-
-    [SyncVar, SerializeField]
+    [SerializeField]
     private PlayerInventory m_playerInventory;
 }

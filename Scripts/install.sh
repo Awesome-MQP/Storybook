41024fc32469440978160a77d8404675d5ead331
bpeake13 <bennypeake@gmail.com>
2015-11-19 19:33:33 -0500

    Travis install test

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,6 +6,3 @@
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
 curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
-
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /

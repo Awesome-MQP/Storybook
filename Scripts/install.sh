5e0aab7f951bbaa83c574bf53d6b909ef71b8a9c
bpeake13 <bennypeake@gmail.com>
2015-11-19 20:24:16 -0500

    Test

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,4 +5,4 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-wget http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+#wget http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg

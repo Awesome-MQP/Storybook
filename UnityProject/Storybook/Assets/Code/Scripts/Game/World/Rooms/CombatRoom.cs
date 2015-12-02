4e0aad34bbd88cea454ecfa3be8513b068a5895d
cporell <cgporell@wpi.edu>
2015-12-01 20:23:29 -0500

    Oops! Apparently I forgot to add the most recent changes. Here they are.

--- UnityProject/Storybook/Assets/Code/Scripts/Game/World/Rooms/CombatRoom.cs
+++ UnityProject/Storybook/Assets/Code/Scripts/Game/World/Rooms/CombatRoom.cs
@@ -1,20 +1,43 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 // This is an empty room. There is nothing special about it.
 // No events will occur upon entering this room.
 public class CombatRoom : RoomObject {
 
+    [SerializeField]
+    private List<GameObject> m_roomEnemiesOverworld = new List<GameObject>();
+    [SerializeField]
+    private EnemyTeam m_roomEnemies;
+
+    private List<Object> m_enemyPawns = new List<Object>();
+
+    private CombatManager m_combatManager;
+    private GameManager m_gameManager;
+
+    private bool m_wonCombat = false;
+
 	// Use this for initialization
 	protected override void Awake ()
     {
         base.Awake();
+        OnRoomEnter();
+        m_gameManager = FindObjectOfType<GameManager>();
 	}
 
     // On entering the room, do nothing since there is nothing special in this room.
     protected override void OnRoomEnter()
     {
         // TODO: Spawn monsters
+        if (!m_wonCombat)
+        {
+            int x = 0, y = 2, z = 1;
+            foreach (GameObject go in m_roomEnemiesOverworld)
+            {
+                m_enemyPawns.Add(Instantiate(go, new Vector3(x, y, z), Quaternion.identity));
+                z-=2;
+            }
+        }
         return;
     }
 
@@ -23,6 +46,8 @@ public class CombatRoom : RoomObject {
     protected override void OnRoomEvent()
     {
         // TODO: Transition into Combat.
+        // Use DungeonMovement for this.
+        m_gameManager.StartCombat();
         return;
     }
 
@@ -30,6 +55,17 @@ public class CombatRoom : RoomObject {
     // Hint: Nothing.
     protected override void OnRoomExit()
     {
+        foreach(Object o in m_enemyPawns)
+        {
+            m_enemyPawns.Remove(o);
+            Destroy(o);
+        }
         return;
+    }
+
+    // Change the value of the room's "won" status
+    public void SetWonStatus(bool won)
+    {
+        m_wonCombat = won;
     }
 }

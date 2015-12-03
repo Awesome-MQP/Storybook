f74c9dd855eaf45e5cee7ef3df6f7a5ffbca6150
nmbryant <nmbryant@wpi.edu>
2015-12-02 19:16:33 -0500

    Task-ItemDropStubs: Creates stub functions for page drops after clearing combat. Some functionality in the method stubs, but not complete.

--- UnityProject/Storybook/Assets/Code/Scripts/Game/Dungeon/DungeonMaster.cs
+++ UnityProject/Storybook/Assets/Code/Scripts/Game/Dungeon/DungeonMaster.cs
@@ -12,6 +12,17 @@ public class DungeonMaster : MonoBehaviour{
     [SerializeField]
     private RoomObject[] m_rooms;
 
+    [SerializeField]
+    private float m_pageSameLevelProbability = 0.7f;
+
+    [SerializeField]
+    private float m_pageLevelPlus1Probability = 0.2f;
+
+    [SerializeField]
+    private float m_pageLevelPlus2Probability = 0.05f;
+
+    [SerializeField]
+    private float m_pageSameGenreProbability = 0.75f;
 
 	// When the DungeonMaster is spawned in the world, have it immediately get all the room prefabs.
 	void Awake () {
@@ -73,5 +84,68 @@ public class DungeonMaster : MonoBehaviour{
         GetRoomPrefab(1, Genre.SciFi, "Curse");
         GetRoomPrefab(4, Genre.Horror, "Shop");
         GetRoomPrefab(2, Genre.Fantasy, "Treasure");
+    }
+
+    /// <summary>
+    /// Function for getting the random page drop from clearing a combat against regular enemies
+    /// TODO: Return a page item once that is created
+    /// </summary>
+    /// <param name="roomGenre">The genre of the room that was cleared</param>
+    /// <param name="roomLevel">The level of the room that was cleared</param>
+    public void GetPageDropFromCombat(Genre roomGenre, int roomLevel)
+    {
+        Genre pageGenre = _getPageGenre(roomGenre);
+        float isPageSameLevel = Random.Range(0.00f, 1.00f);
+        if (isPageSameLevel <= m_pageSameLevelProbability)
+        {
+            //TODO: Create a page of the same level and return it
+            return;
+        }
+
+        float isPageLevelPlus1 = Random.Range(0, 1);
+        if (isPageLevelPlus1 <= m_pageLevelPlus1Probability)
+        {
+            //TODO: Create a page with one level higher than the given room level
+            return;
+        }
+
+        float isPageLevelPlus2 = Random.Range(0, 1);
+        if (isPageLevelPlus2 <= m_pageLevelPlus2Probability)
+        {
+            //TODO: Create a page with two levels higher than the given room level
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Randomly selects a page drop for a boss
+    /// </summary>
+    /// <param name="bossGenre"></param>
+    public void GetPageDropFromBoss(Genre bossGenre, int bossLevel)
+    {
+        Genre pageGenre = _getPageGenre(bossGenre);
+    }
+
+    /// <summary>
+    /// Randomly selects a genre for a page drop based on the 'pageSameGenreProbability' value
+    /// Should be a higher chance of creating a page of the favored genre
+    /// If it is not the favored genre, all other genres have an equal chance of being chosen
+    /// </summary>
+    /// <param name="favoredGenre">The genre that will be favored and have a higher chance of being selected</param>
+    /// <returns>The genre that has been chosen for the page drop</returns>
+    private Genre _getPageGenre(Genre favoredGenre)
+    {
+        Genre pageGenre = favoredGenre;
+        float genreRandomizer = Random.Range(0.00f, 1.00f);
+        if (genreRandomizer >= m_pageSameGenreProbability)
+        {
+            Genre[] allGenresArray = { Genre.Fantasy, Genre.GraphicNovel, Genre.Horror, Genre.SciFi };
+            List<Genre> allGenres = new List<Genre>(allGenresArray);
+            allGenres.Remove(favoredGenre);
+            int genreIndex = Random.Range(0, allGenres.Count);
+            pageGenre = allGenres[genreIndex];
+        }
+
+        return pageGenre;
     }
 }

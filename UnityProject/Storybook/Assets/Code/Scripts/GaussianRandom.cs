0171faad03c37a6512f378810a5e6f406c6fdcc1
nmbryant <nmbryant@wpi.edu>
2016-01-14 00:41:51 -0500

    Adds a function for generating random Gaussian numbers

---
+++ UnityProject/Storybook/Assets/Code/Scripts/GaussianRandom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GaussianRandom : System.Random {
+
+    public static double GetRandomNumber()
+    {
+        float u1, u2, z;
+        System.Random r = new System.Random();
+        
+        u1 = float.Parse(r.NextDouble().ToString());
+        u2 = float.Parse(r.NextDouble().ToString());
+
+        z = Mathf.Sqrt(-2 * Mathf.Log(u1, 2)) * Mathf.Sin(2 * Mathf.PI * u2);
+
+        return z;
+    }
+	
+}
---
+++ UnityProject/Storybook/Assets/Code/Scripts/TestGaussianRandom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGaussianRandom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("TEST");
+        double randomNumber = GaussianRandom.GetRandomNumber();
+        Debug.Log("Gaussian Random Number = " + randomNumber);
+	}
+}

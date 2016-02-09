7b26929d9cf516838218332eb7a7dde1a0ac6658
nmbryant <nmbryant@wpi.edu>
2016-02-09 00:04:07 -0500

    GameOver and WinScreen UIs now destroy all PlayerEntity objects left over in the scene so that a new game can start properly.

--- UnityProject/Storybook/Assets/Code/Scripts/Core/Input/GameOverUIHandler.cs
+++ UnityProject/Storybook/Assets/Code/Scripts/Core/Input/GameOverUIHandler.cs
@@ -4,5 +4,14 @@ using System;
 
 public class GameOverUIHandler : GameMenuUIHandler
 {
-
+    
+    void Start()
+    {
+        PlayerObject[] allPlayerObjects = FindObjectsOfType<PlayerObject>();
+        foreach(PlayerObject po in allPlayerObjects)
+        {
+            Destroy(po.photonView);
+            Destroy(po.gameObject);
+        }
+    }
 }
--- UnityProject/Storybook/Assets/Code/Scripts/Core/Input/WinScreenUIHandler.cs
+++ UnityProject/Storybook/Assets/Code/Scripts/Core/Input/WinScreenUIHandler.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class WinScreenUIHandler : GameMenuUIHandler {
 
-
+    void Start()
+    {
+        PlayerObject[] allPlayerObjects = FindObjectsOfType<PlayerObject>();
+        foreach (PlayerObject po in allPlayerObjects)
+        {
+            Destroy(po.photonView);
+            Destroy(po.gameObject);
+        }
+    }
 
 }

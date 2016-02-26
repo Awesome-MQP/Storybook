a6051940dfa23e5e7a78d61fb23ef7e56349dbad
cporell <cgporell@wpi.edu>
2016-02-26 16:50:59 -0500

    Finishes work on pre-game lobbies. Players can join an existing lobby or can create a new one (if a lobby of that name isn't found). From there, players are taken to a pre-game lobby screen showing all players in the room. The host may start the game whenever they wish, other players must wait for the host to do so.

---
+++ UnityProject/Storybook/Assets/Code/Scripts/Core/Input/PreGameLobbyUIHandler.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class PreGameLobbyUIHandler : Photon.PunBehaviour {
+
+    private Text m_currentPlayersInRoomText;
+    private String m_startGameText = "Start the Game!";
+    private String m_waitForHostText = "Waiting for Host to Start...";
+
+    public void Start()
+    {
+        _setPlayersText();
+        _setNameOfLobbyText();
+        Button startButton = GetComponentInChildren<Button>();
+        if(!PhotonNetwork.isMasterClient)
+        {
+            // if not the host, disable the Start Game button
+            startButton.interactable = false;
+            startButton.GetComponentInChildren<Text>().text = m_waitForHostText;
+        }
+        else
+        {
+            // if the host, enable the ability to start game
+            startButton.interactable = true;
+            startButton.GetComponentInChildren<Text>().text = m_startGameText;
+        }
+    }
+
+    private void _setNameOfLobbyText()
+    {
+        Text[] text = GetComponentsInChildren<Text>();
+        foreach (Text t in text)
+        {
+            if (t.name == "GameName")
+            {
+                t.text = PhotonNetwork.room.name;
+                return;
+            }
+        }
+    }
+
+    private void _setPlayersText()
+    {
+        Text[] text = GetComponentsInChildren<Text>();
+        foreach (Text t in text)
+        {
+            if (t.name == "PlayersInLobby")
+            {
+                m_currentPlayersInRoomText = t;
+                return;
+            }
+        }
+    }
+
+    public void onStartButtonClicked()
+    {
+        if(PhotonNetwork.isMasterClient)
+        {
+            // only load if we are the master client
+            PhotonNetwork.LoadLevel("CharacterSelect");
+        }
+    }
+
+    public void OnGUI()
+    {
+        // Populate the current players in lobby list
+        // Do this once per cycle to keep the list up to date
+        m_currentPlayersInRoomText.text = "";
+        foreach (PhotonPlayer player in PhotonNetwork.playerList)
+        {
+            m_currentPlayersInRoomText.text += player.ID + " " + player.name + "\n";
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}

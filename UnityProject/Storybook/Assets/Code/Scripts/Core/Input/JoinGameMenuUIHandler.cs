2e184db179fe57e2a2562e87c42a35614881595f
cporell <cgporell@wpi.edu>
2016-03-20 18:52:06 -0400

    Cleaned up some pregame UIs and made searching for games a bit easier to do.

--- UnityProject/Storybook/Assets/Code/Scripts/Core/Input/JoinGameMenuUIHandler.cs
+++ UnityProject/Storybook/Assets/Code/Scripts/Core/Input/JoinGameMenuUIHandler.cs
@@ -7,34 +7,60 @@ using System;
 
 public class JoinGameMenuUIHandler : Photon.PunBehaviour {
 
+    private ScrollRect m_availGamesRect;
+    [SerializeField]
+    private float m_gridXPadding;
+    [SerializeField]
+    private float m_gridYPadding;
+    [SerializeField]
+    private float m_buttonWidth;
+    [SerializeField]
+    private float m_buttonHeight;
+    [SerializeField]
+    private GameInfoButton m_gameInfoButton;
+
     private Text m_availGamesText;
 
     public void Start()
     {
-        _setAvailableGamesText();
+        //_setAvailableGamesText();
+        _setAvailableGamesScrollRect();
     }
 
     public void Update()
     {
         // Populate the available games list
-        m_availGamesText.text = "";
+        //m_availGamesText.text = "";
+        RectTransform gamesContent = m_availGamesRect.content;
         foreach(RoomInfo game in PhotonNetwork.GetRoomList())
         {
-            m_availGamesText.text += game.name + " " + game.playerCount + "/" + game.maxPlayers + "\n";
+            String gameInfo = game.name + " | Players in lobby: " + game.playerCount + " / " + game.maxPlayers;
+            GameInfoButton lobbyButton = Instantiate(m_gameInfoButton);
+            lobbyButton.GetComponent<Text>().text = gameInfo;
+            lobbyButton.transform.SetParent(gamesContent, false);
+            //m_availGamesText.text += game.name + " " + game.playerCount + "/" + game.maxPlayers + "\n";
         }
+        /*
         if(PhotonNetwork.GetRoomList().Length == 0)
         {
             m_availGamesText.text = "No games found!";
         }
+        */
+    }
+
+    public void OnLobbyButtonClick(String lobbyname)
+    {
+        _joinLobby(lobbyname);
     }
 
     public void OnSearchButtonClick()
     {
-        
         InputField search = GetComponentInChildren<InputField>();
         String searchText = search.text;
-
+        _joinLobby(searchText);
+        
         // Only search for a game if 
+        /*
         if(!String.IsNullOrEmpty(searchText))
         {
             bool foundRoom = false;
@@ -61,6 +87,38 @@ public class JoinGameMenuUIHandler : Photon.PunBehaviour {
                 PhotonNetwork.CreateRoom(searchText, options, null);
             }
         }
+        */
+    }
+
+    private void _joinLobby(String lobbyName)
+    {
+        // Only search for a game if 
+        if (!String.IsNullOrEmpty(lobbyName))
+        {
+            bool foundRoom = false;
+
+            foreach (RoomInfo room in PhotonNetwork.GetRoomList())
+            {
+                if (room.name == lobbyName)
+                {
+                    PhotonNetwork.JoinRoom(lobbyName);
+                    foundRoom = true;
+                    break;
+                }
+            }
+            // if the room exists, join it
+            if (foundRoom == true)
+            {
+                PhotonNetwork.JoinRoom(lobbyName);
+            }
+            // otherwise make a new room
+            if (foundRoom == false)
+            {
+                RoomOptions options = new RoomOptions();
+                options.maxPlayers = 4;
+                PhotonNetwork.CreateRoom(lobbyName, options, null);
+            }
+        }
     }
 
     public override void OnJoinedRoom()
@@ -73,6 +131,19 @@ public class JoinGameMenuUIHandler : Photon.PunBehaviour {
         }
 
         SceneManager.LoadScene("PreGameLobby");
+    }
+
+    private void _setAvailableGamesScrollRect()
+    {
+        ScrollRect scrollrect = GetComponentInChildren<ScrollRect>();
+        GridLayoutGroup gridGroup = scrollrect.GetComponentInChildren<GridLayoutGroup>();
+        if (gridGroup != null)
+        {
+            gridGroup.cellSize = new Vector2(m_buttonWidth, m_buttonHeight);
+            gridGroup.spacing = new Vector2(m_gridXPadding, m_gridYPadding);
+        }
+
+        m_availGamesRect = scrollrect;
     }
 
     private void _setAvailableGamesText()
---
+++ UnityProject/Storybook/Assets/Code/Scripts/Game/JoinCreateGame/JoinCreateGameStartup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoinCreateGameStartup : MonoBehaviour {
+    [SerializeField]
+    private JoinGameMenuUIHandler m_joinCreateGamePrefab;
+
+	// Use this for initialization
+	protected void Awake ()
+    {
+        JoinGameMenuUIHandler ui = Instantiate(m_joinCreateGamePrefab);
+	}
+
+    protected void Start()
+    {
+        
+    }
+}

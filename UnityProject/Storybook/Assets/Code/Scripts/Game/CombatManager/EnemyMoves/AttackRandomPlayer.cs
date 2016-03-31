2f99a4df702c54bcd88048b41f9523a37207225f
Bryant <nmbryant@wpi.edu>
2016-03-31 11:11:48 -0400

    Creates an enemy attack move that chooses a random target. Currently only given to one of the comic book enemies.

---
+++ UnityProject/Storybook/Assets/Code/Scripts/Game/CombatManager/EnemyMoves/AttackRandomPlayer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AttackRandomPlayer : AIMove {
+
+    private bool IS_MOVE_ATTACK = true;
+
+    private System.Random rng = new System.Random();
+
+    void Start()
+    {
+        SetIsMoveAttack(IS_MOVE_ATTACK);
+    }
+
+    /// <summary>
+    /// Deals combat damage to all the player pawns in the target list
+    /// </summary>
+    protected override void DoMoveEffect()
+    {
+        foreach (CombatPawn combatPawn in MoveTargets)
+        {
+            int moveDamage = StatsManager.CalcDamage(MoveOwner.PawnGenre, combatPawn.PawnGenre, MoveGenre, MoveLevel, MoveOwner.Attack, combatPawn.Defense);
+            Debug.Log("Enemy dealing " + moveDamage + " to the player");
+            combatPawn.DealDamageToPawn(moveDamage);
+        }
+    }
+
+    /// <summary>
+    /// Chooses the targets for the move, currently chooses the pawns that have the lowest health
+    /// </summary>
+    /// <param name="possibleTargets">The list of CombatPawn that are the possible targets for the move</param>
+    public override void ChooseTargets(HashSet<CombatPawn> possibleTargets)
+    {
+        // Reset the targets list before starting
+        SetMoveTargets(new List<CombatPawn>());
+
+        base.ChooseTargets(possibleTargets);
+        if (MoveTargets.Length > 0)
+        {
+            return;
+        }
+
+        List<CombatPawn> possibleTargetsList = new List<CombatPawn>(possibleTargets);
+        List<CombatPawn> targets = new List<CombatPawn>();
+
+        while (targets.Count < NumberOfTargets)
+        {
+            int randomPawnIndex = rng.Next(0, possibleTargetsList.Count);
+
+            targets.Add(possibleTargetsList[randomPawnIndex]);
+            possibleTargetsList.RemoveAt(randomPawnIndex);
+        }
+        SetMoveTargets(targets);
+    }
+
+    public override void InitializeIsMoveAttack()
+    {
+        SetIsMoveAttack(IS_MOVE_ATTACK);
+    }
+}

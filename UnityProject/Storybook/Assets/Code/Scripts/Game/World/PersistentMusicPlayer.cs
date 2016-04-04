8030dc2e94373c22eb1964a0494b56c875e63411
cporell <cgporell@wpi.edu>
2016-04-03 23:27:25 -0400

    Added a PersistentMusicMgr that plays the Title theme across scenes until the main game loads.

--- UnityProject/Storybook/Assets/Code/Scripts/Game/World/PersistentMusicPlayer.cs
+++ UnityProject/Storybook/Assets/Code/Scripts/Game/World/PersistentMusicPlayer.cs
@@ -4,20 +4,44 @@ using System.Collections;
 // A class that is meant to play music across multiple scenes (i.e. the main theme)
 public class PersistentMusicPlayer : MonoBehaviour {
 
+    [SerializeField]
+    private AudioSource m_source;
+    [SerializeField]
+    private AudioClip m_titleTheme;
 
+    private static PersistentMusicPlayer s_instance;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+    public static PersistentMusicPlayer Instance
+    {
+        get { return s_instance; }
+    }
+
+    // Use this for initialization
+    protected void Awake () {
+        s_instance = this;
+        DontDestroyOnLoad(this);
+        m_source.clip = m_titleTheme;
+        m_source.Play();
 	}
 
     void OnLevelWasLoaded(int level)
     {
-        
+        switch(level)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                Destroy(this.gameObject);
+                break;
+        }
     }
 }
